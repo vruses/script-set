3f915f15c2cb94f33688cd1376cae92b548e16e6
vurses <vurses@qq.com>
2025-06-23 01:19:50 +0800

    refactor: 根据规范重构下promise

--- promiseLike.js
+++ promiseLike.js
@@ -1,69 +1,86 @@
 class PromiseLike {
   static resolve(result) {
-    // 如果是通过构造函数调用,同时状态只能由pending变为res或rej
-    if (this instanceof PromiseLike && this.PromiseState === "pending") {
-      // console.log(this.constructor);
-      // console.log("reslove: " + result);
-      this.PromiseState = "fulfilled";
-      this.PromiseResult = result;
-      console.log(this.PromiseState);
-    } else {
-      // 通过类调用
-      if (result instanceof PromiseLike) {
-        return result;
-      }
-      return new PromiseLike((res) => {
-        res(result);
-      });
+    // 通过类调用的静态方法
+    if (result instanceof PromiseLike) {
+      return result;
     }
+    return new PromiseLike((res) => {
+      res(result);
+    });
   }
+
   static reject(result) {
-    if (this instanceof PromiseLike && this.PromiseState === "pending") {
-      // console.log("reject:" + this.PromiseState);
-
-      // console.log("reject:" + result);
-      this.PromiseState = "rejected";
-      this.PromiseResult = result;
-      console.log(this.PromiseState);
-    } else {
-      if (result instanceof PromiseLike) {
-        return result;
-      }
-      return new PromiseLike((_res, rej) => {
-        rej(result);
-      });
+    // 通过类调用的静态方法
+    if (result instanceof PromiseLike) {
+      return result;
     }
+    return new PromiseLike((_res, rej) => {
+      rej(result);
+    });
+  }
 
-    // console.error((new Error('123').name = "Uncaught (in promise)"));
+  constructor(executor) {
+    this.state = "pending";
+    this.value = undefined;
+    this.reason = undefined;
+
+    // 构造函数里面添加两个数组存储成功和失败的回调
+    this.onFulfilledCallbacks = [];
+    this.onRejectedCallbacks = [];
+
+    const resolve = (value) => {
+      // 只有在 pending 状态时才能改变状态
+      if (this.state === "pending") {
+        this.state = "fulfilled";
+        this.value = value;
+        this.onFulfilledCallbacks.forEach((callback) => {
+          // 保证访问值得一致性
+          callback(this.value);
+        });
+        console.log("状态变为:", this.state, "结果:", this.value);
+      } else {
+        console.log("状态已锁定，无法从", this.state, "变为 fulfilled");
+      }
+    };
+
+    const reject = (reason) => {
+      // 只有在 pending 状态时才能改变状态
+      if (this.state === "pending") {
+        this.state = "rejected";
+        this.reason = reason;
+        console.log(this.reason);
+        console.log("状态变为:", this.state, "结果:", this.reason);
+      } else {
+        console.log("状态已锁定，无法从", this.state, "变为 rejected");
+      }
+    };
+
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      console.error(`Uncaught (in promise)`,`${this.reason}`);
+    }
   }
-  constructor(func) {
-    this.PromiseState = "pending";
-    this.PromiseResult = undefined;
-    func(
-      this.constructor.resolve.bind(this),
-      this.constructor.reject.bind(this)
-    );
-  }
-  then(func) {
-    switch (this.PromiseState) {
-      case "pending":
-        return this;
-      case "fulfilled":
-        func(this.PromiseResult);
-        return PromiseLike.resolve(this.PromiseResult);
-      case "rejected":
-        return PromiseLike.reject(this.PromiseResult);
+
+  then(onFulfilled,onRejected) {
+    // 可能传入非函数
+    // 不同state做出不同行为
+    if (this.state === "fulfilled") {
+      onFulfilled(this.value);
+    } else if (this.state === "rejected") {
+      onRejected(this.reason);
+    } else {
+      this.onFulfilledCallbacks.push(executor);
+      this.onRejectedCallbacks.push(executor);
     }
   }
 }
-// const promise_like = new PromiseLike((res, rej) => {
-//   res({ foo: 123 });
-// });
 const promise_lik2 = new PromiseLike((res, rej) => {
   setTimeout(() => {
     res(123);
     rej(321);
   }, 2000);
+}).then(res=>{
+  console.log('then=>',res)
 });
-// console.log(promise_lik2);
-// const foo = PromiseLike.resolve(PromiseLike.resolve(123));
+console.log(promise_lik2);
